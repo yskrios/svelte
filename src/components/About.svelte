3ac0c3691ead0fdb0e8a57d25c0eb88acd95bfdc
Yessica Ríos <yskrios@gmail.com>
2021-04-01 00:35:59 -0500

    Añadiendo una funcion

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,11 +1,17 @@
 <script>
   let someText = "Frontend Developer at Globant";
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
 </script>
 
 <div class="About">
   <p>
     {someText}
   </p>
+  <button on:click={handleClick}> Click{count === 0 ? "" : count} </button>
 </div>
 
 <style>
